d6e9332313339496ea2e3a81fc069e0dfe0d29b6
Rofo <88415585+rudycito23@users.noreply.github.com>
2022-01-09 16:54:02 -0600

    Update HeartRate.h
    added comments in class HeartRates

--- HeartRate.h
+++ HeartRate.h
@@ -17,52 +17,67 @@ class HeartRates {
         birthDay;
         birthYear;    
     }
+    // set user first name
     void setFirstName(string personFirstName) {
         firstName = personFirstName;
     }
+    // retrieve user first name
     string getFirstName() {
         return firstName;
     }
+    // set user last name
     void setLastName(string personLastName) {
         lastName = personLastName;
     }
+    // get user last name
     string getLastName() {
         return lastName;
     }
+    // set user birth month
     void setBirthMonth(int month) {
         birthMonth = month;
     }
+    // get user birth month
     int getBirthMonth() {
         return birthMonth;
     }
+    // set user birth day
     void setBirthDay(int day) {
         birthDay = day;
     }
+    // get user birth day
     int getBirthDay() {
         return birthDay;
     }
+    // set user birth year
     void setBirthYear(int year) {
         birthYear = year;
     }
+    // set user birth year
     int getBirthYear() {
         return birthYear;
     }
+    // calculate and return user age
     int getAge() {
         age = 2022 - birthYear;
         return age;
     }
+    // calculate and return user's max heart rate
     int getMaxHeartRate() {
         heartRate = 220 - age;
         return heartRate;
     }
+    // calculate and return user's target heart rate at 50% of max heart rate
     int getTargetHeartRate50() {
         targetHeartRate50 = heartRate * .50;
         return targetHeartRate50;
     }
+    // calculate and return user's target heart rate at 85% of max heart rate
     int getTargetHeartRate85() {
         targetHeartRate85 = heartRate * .85;
         return targetHeartRate85;
     }
+    // print user's information
     void printAll() {
         cout << getFirstName() << " " << getLastName() << endl;
         cout << "DOB: " << getBirthMonth() << "/" << getBirthDay() << "/" << getBirthYear() << endl;
