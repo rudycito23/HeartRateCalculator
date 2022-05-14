26ea78599d324b8abf2d8591c8d1a75fadc313c1
rudycito23 <88415585+rudycito23@users.noreply.github.com>
2022-05-14 11:41:50 -0500

    added comments to HeartRates class; updated README

--- HeartRates.cpp
+++ HeartRates.cpp
@@ -4,62 +4,60 @@
 
 #include "HeartRates.h"
 
+// constructor
 HeartRates::HeartRates() {
     firstName = "No first name";
     lastName = "No last name";
-    birthMonth;
-    birthDay;
-    birthYear;
 }
-
+// set the user's first name
 void HeartRates::setFirstName(string first_name) {
     firstName = first_name;
 }
-
+// get the user's first name
 string HeartRates::getFirstName() {
     return firstName;
 }
-
+// set the user's last name
 void HeartRates::setLastName(string last_name) {
     lastName = last_name;
 }
-
+// get the user's last name
 string HeartRates::getLastName() {
     return lastName;
 }
-
+// set the user's birth month
 void HeartRates::setBirthMonth(int month) {
     birthMonth = month;
 }
-
+// get the user's birth month
 int HeartRates::getBirthMonth() {
     return birthMonth;
 }
-
+// set the user's birth day
 void HeartRates::setBirthDay(int day) {
     birthDay = day;
 }
-
+// get the user's birth day
 int HeartRates::getBirthDay() {
     return birthDay;
 }
-
+// set the user's birth year
 void HeartRates::setBirthYear(int year) {
     birthYear = year;
 }
-
+// get the user's birth year
 int HeartRates::getBirthYear() {
     return birthYear;
 }
-
+// set the current year
 void HeartRates::setCurrentYear(int presentYear) {
     currentYear = presentYear;
 }
-
+// get the current year
 int HeartRates::getCurrentYear() {
     return currentYear;
 }
-
+// convert the user's numerical birth month into its formal spelling
 string HeartRates::getDOB() {
     string month;
     if ((birthMonth > 0) && (birthMonth < 13)) {
@@ -94,29 +92,29 @@ string HeartRates::getDOB() {
     }
     return month;
 }
-
+// calculate the user's age
 int HeartRates::getAge() {
     int age = getCurrentYear() - getBirthYear();
     return age;
 }
-
+// calculate the user's max heart rate
 int HeartRates::maxHeartRate() {
     int maxRate = 220 - getAge();
     return maxRate;
 }
-
+// calculate the user's target heart rate at 50% of max heart rate
 int HeartRates::targetHeartRate50() {
     int targetRate50 = maxHeartRate() * .50;
 
     return targetRate50;
 }
-
+// calculate the user's target heart rate at 85% of max heart rate
 int HeartRates::targetHeartRate85() {
     int targetRate85 = maxHeartRate() * .85;
 
     return targetRate85;
 }
-
+// Create and output the UI to collect the user's info
 void HeartRates::printAll() {
     string name;
     int number;
@@ -145,5 +143,6 @@ void HeartRates::printAll() {
     cout << "Age: " << getAge() << endl;
     cout << "Your max heart rate: " << maxHeartRate() << " beats per minute" << endl;
     cout << "Your target heart rate is 50-85% of your max heart rate: " << targetHeartRate50() << " - " << targetHeartRate85()
-            << " beats per minute" << endl;
+            << " beats per minute" << endl << endl;
+    cout << "Thank you for using myHeartRate Calculator!";
 }
